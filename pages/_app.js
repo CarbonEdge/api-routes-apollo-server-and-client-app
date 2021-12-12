c9b98dbd40101dc6bbdfee480f6640db25d18b18
CarbonEdge <wholetree65@gmail.com>
2021-12-12 21:49:59 +0200

    add style sheet

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import { ApolloProvider } from '@apollo/client'
 import { useApollo } from '../apollo/client'
 import Navbar from '../components/ui/Navbar/Navbar'
-// import '../components/ui/Navbar/Navbar.css'
+import './styles.css'
 
 export default function App({ Component, pageProps }) {
   const apolloClient = useApollo(pageProps.initialApolloState)
