dea8b749c08ef1aef250876f981f684e768ff309
Melvin Chotu <melvin@agatha.co.za>
2021-12-21 23:21:54 +0200

    add home component

---
+++ components/ui/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div>
+                This is the home page and description
+            </div>
+        )
+    }
+}
+
+export default Home;
